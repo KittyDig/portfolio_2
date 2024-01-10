15c0313539b9886dc68ddbcab8c54e27cd506cb3
KittyDig <kittydignam@gmail.com>
2024-01-10 21:00:18 +0000

    Add javascript elements

---
+++ script.js
@@ -0,0 +1,7 @@
+//get elements
+let ball = document.getElementById('ball');
+let paddle1 = document.getElementById('paddle1');
+let paddle2 = document.getElementById('paddle2');
+let player1Score = document.getElementById('player1-score');
+let player2Score = document.getElementById('player2-score');
+let timerDisplay = document.getElementById('timer');
