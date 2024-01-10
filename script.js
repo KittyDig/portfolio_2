33a2fc4052408a2a8517c819e9f1f8aec15e7d77
KittyDig <kittydignam@gmail.com>
2024-01-10 21:05:53 +0000

    Add functions

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ let paddle2 = document.getElementById('paddle2');
 let player1Score = document.getElementById('player1-score');
 let player2Score = document.getElementById('player2-score');
 let timerDisplay = document.getElementById('timer');
+
+function startGame() {
+}
+function updateGame() {
+}
+function movePaddles() {
+}
+function updatePaddlePosition() {
+}
+function moveBall() {
+}
+function checkCollision() {
+}
+function updateScore() {
+}
+function endGame() {
+}
+function updateTimer() {
+}
