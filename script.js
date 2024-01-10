2517914296c3dc3d54d66e5690c2a7c1351f2285
KittyDig <kittydignam@gmail.com>
2024-01-10 21:27:50 +0000

    DOMContentLoaded funtion added to make sure the HTML document has been completely loaded first

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
 //get elements
 let ball = document.getElementById('ball');
 let paddle1 = document.getElementById('paddle1');
@@ -5,7 +7,9 @@ let paddle2 = document.getElementById('paddle2');
 let player1Score = document.getElementById('player1-score');
 let player2Score = document.getElementById('player2-score');
 let timerDisplay = document.getElementById('timer');
+}
 
+//functions
 function startGame() {
 }
 function updateGame() {
