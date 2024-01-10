72a02548c3e6fe23c86fcea5807c6b65718f6526
KittyDig <kittydignam@gmail.com>
2024-01-10 22:57:18 +0000

    Added the initial game state

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ let paddle2 = document.getElementById('paddle2');
 let player1Score = document.getElementById('player1-score');
 let player2Score = document.getElementById('player2-score');
 let timerDisplay = document.getElementById('timer');
+
+//sets initial game state
+let winCondition = 3;
+let seconds = 0;
+let gameTimer;
+let ballX = Math.random() * 400;
+let ballY = 100;
+let ballSpeedX = 3;
+let ballSpeedY = 2;
+let paddle1Y = 80;
+let paddle2Y = 80;
+let paddle2Speed = 1;
 }
 
 //functions
